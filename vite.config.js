e5c62f825fe3d50a3eb5a5c3fe89329ed177df80
Tomás <taplatero@outlook.es>
2025-02-20 12:32:21 +0100

    added acept externals connections

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 8080, // Usa el puerto de Railway o 8080 como fallback
+    host: "0.0.0.0" // Permite conexiones externas
+  }
 })
